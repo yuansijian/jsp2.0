23551b3a20076c935ad57b791dc40aa47bb17d16
yuansijian <1789670108@qq.com>
2019-04-13 13:05:42 +0800

    将问卷答案存入数据

---
+++ src/storage.java
@@ -0,0 +1,121 @@
+//问卷的javaBean
+package com;
+
+public class storage {
+    private String schoolName;
+    private String sex;
+    private String age;
+    private String have;
+    private String have1;
+    private String location;
+    private String reasons;
+    private String reason1s;
+    private String thinkings;
+    private String treats;
+    private String dos;
+    private String know;
+    private String feeling;
+    private String evaluations;
+
+    public storage(){
+        super();
+    }
+
+    public void setSchoolName(String schoolName){
+        this.schoolName = schoolName;
+    }
+    public String getSchoolName(){
+        return schoolName;
+    }
+
+    public void setSex(String sex){
+        this.sex = sex;
+    }
+    public String getSex(){
+        return sex;
+    }
+
+    public void setAge(String age){
+        this.age = age;
+    }
+    public String getAge(){
+        return age;
+    }
+
+    public void setHave(String have){
+        this.have = have;
+    }
+    public String getHave(){
+        return have;
+    }
+
+    public void setHave1(String have1){
+        this.have1 = have1;
+    }
+    public String getHave1(){
+        return have1;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+    public String getLocation(){
+        return location;
+    }
+
+    public void setReasons(String reasons){
+        this.reasons = reasons;
+    }
+    public String getReasons(){
+        return reasons;
+    }
+
+    public void setReason1s(String reason1s){
+        this.reason1s = reason1s;
+    }
+    public String getReason1s(){
+        return reason1s;
+    }
+
+    public void setThinkings(String thinkings){
+        this.thinkings = thinkings;
+    }
+    public String getThinkings(){
+        return thinkings;
+    }
+
+    public void setTreats(String treats){
+        this.treats = treats;
+    }
+    public String getTreats(){
+        return treats;
+    }
+
+    public void setDos(String dos){
+        this.dos = dos;
+    }
+    public String getDos(){
+        return dos;
+    }
+
+    public void setKnow(String know){
+        this.know = know;
+    }
+    public String getKnow(){
+        return know;
+    }
+
+    public void setFeeling(String feeling){
+        this.feeling = feeling;
+    }
+    public String getFeeling(){
+        return feeling;
+    }
+
+    public void setEvaluations(String evaluations){
+        this.evaluations = evaluations;
+    }
+    public String getEvaluations(){
+        return evaluations;
+    }
+}
